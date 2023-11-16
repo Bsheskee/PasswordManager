069076f3d374beb31c8da5cd29d3ebbc032cb17e
Bsheskee <bartlomiejbialobrzewski@gmail.com>
2023-11-16 15:37:22 +0100

    Initial Commit

---
+++ PasswordManager/PasswordManager.h
@@ -0,0 +1,18 @@
+//
+//  PasswordManager.h
+//  PasswordManager
+//
+//  Created by bartek on 16/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PasswordManager.
+FOUNDATION_EXPORT double PasswordManagerVersionNumber;
+
+//! Project version string for PasswordManager.
+FOUNDATION_EXPORT const unsigned char PasswordManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasswordManager/PublicHeader.h>
+
+
